7c66fc30b02a5afb1d57fd81ae4b2d385287c68d
pallavikyama <kyamap98@gmail.com>
2020-10-15 19:10:39 +0530

    Added:Given a date the cheapest and best rated hotel available for a reward customer are printed.

--- test/HotelReservationTest01.java
+++ test/HotelReservationTest01.java
@@ -2,14 +2,14 @@ package com.blz.hotelreservationsystem.test;
 
 import org.junit.Test;
 import com.blz.hotelreservationsystem.main.HotelReservation;
-import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HotelReservationTest01 {
 
-	// ADD SPECIAL RATES FOR REWARD CUSTOMERS ON WEEKDAYS AND WEEKENDS IN EACH HOTEL
+	// REWARD CUSTOMER DATE RANGE ENTRY GIVES CHEAPEST, BEST RATED HOTEL AVAILABLE
 	@Test
-	public void hotelTestUC09() {
+	public void hotelTestUC10() {
 		ArrayList<HotelReservation> hotelList = new ArrayList<>();
 		HotelReservation hotel1 = new HotelReservation("Lakewood", 110, 90, 80, 80, 3);
 		HotelReservation hotel2 = new HotelReservation("Bridgewood", 150, 50, 110, 50, 4);
@@ -17,11 +17,25 @@ public class HotelReservationTest01 {
 		hotelList.add(hotel1);
 		hotelList.add(hotel2);
 		hotelList.add(hotel3);
-		assertTrue(hotelList.get(0).getRewardCustomerRateOnWeekday() == 80);
-		assertTrue(hotelList.get(1).getRewardCustomerRateOnWeekday() == 110);
-		assertTrue(hotelList.get(2).getRewardCustomerRateOnWeekday() == 100);
-		assertTrue(hotelList.get(0).getRewardCustomerRateOnWeekend() == 80);
-		assertTrue(hotelList.get(1).getRewardCustomerRateOnWeekend() == 50);
-		assertTrue(hotelList.get(2).getRewardCustomerRateOnWeekend() == 40);
+		Scanner input = new Scanner(System.in);
+		boolean x;
+		do {
+			System.out.println("Enter Date-Range to get the cheapest hotel available:"
+					+ "\n(Enter in (ddMMMyyyy) format only, seperated with a comma(,))");
+			try {
+				x = true;
+				HotelReservation.timeFormat(input.nextLine());
+			} catch (Exception e) {
+				x = false;
+				System.out.println("Entered invalid dates. Try again.");
+			}
+		} while (!x);
+		ArrayList<HotelReservation> cheapestHotels = HotelReservation.findCheapestHotel(hotelList);
+		double totalRate = cheapestHotels.get(0).getRewardCustomerRateOnWeekday()
+				* HotelReservation.numOfWeekdayBookings
+				+ cheapestHotels.get(0).getRewardCustomerRateOnWeekend() * HotelReservation.numOfWeekendBookings;
+		System.out.print(HotelReservation.findBestRatedHotel(cheapestHotels).getHotelName() + "    Rating:"
+				+ HotelReservation.findBestRatedHotel(cheapestHotels).getRating() + "    Total Rate:" + totalRate);
+		input.close();
 	}
 }
